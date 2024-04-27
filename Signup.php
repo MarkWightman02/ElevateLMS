e319b4d4012800a03c11301199b2ec744ffcb0af
mark_wightman <mark_wightman@uri.edu>
2024-04-27 19:29:25 -0400

    Continutity with capitalization for importing to cpanel

--- Signup.php
+++ Signup.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style/style.css">
+	<link rel="stylesheet" href="Style/style.css">
     <title>Add User</title>
 </head>
 <body>
